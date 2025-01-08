53fdf86283b95a8dc26bd470a4b05443066c387f
v1r7ual83 <v1r7ual@tutanota.com>
2025-01-08 14:59:40 +0000

    Create get_reviews_by_park method in Process class.

--- process.py
+++ process.py
@@ -34,3 +34,7 @@ class Process:
             if branch and branch not in branches:
                 branches.append(branch)
         return branches
+
+    @staticmethod
+    def get_reviews_by_park(branch_name, reviews):
+        return [review for review in reviews if review[4] == branch_name]
