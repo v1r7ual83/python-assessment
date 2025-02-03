b438847f9ba355ef9af23261c90d6c017c2ff482
v1r7ual83 <v1r7ual@tutanota.com>
2025-02-03 16:02:53 +0000

    Create reviewer_locations property.

--- exporter.py
+++ exporter.py
@@ -3,6 +3,14 @@ class Park:
         self.name = name
         self.reviews = {}
 
+    @property
+    def reviewer_locations(self):
+        locations = []
+        for review in self.reviews.values():
+            if review.reviewer_location not in locations:
+                locations.append(review.reviewer_location)
+        return locations
+
 class Review:
     def __init__(self, review_id, rating, date, reviewer_location, park):
         self.review_id = review_id
